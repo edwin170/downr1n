bbaaa0ede5e8c4a33fe75b9080b4ae1af74f0675
ささ <91324887+sasa8810@users.noreply.github.com>
2023-08-31 19:14:37 +0900

    don't remove linux_fix.sh

--- downr1n.sh
+++ downr1n.sh
@@ -448,7 +448,6 @@ if [ ! -e "$oscheck"/futurerestore ]; then
         curl -sLo futurerestore-Linux-x86_64-RELEASE.zip https://nightly.link/futurerestore/futurerestore/workflows/ci/main/futurerestore-Linux-x86_64-RELEASE.zip
         unzip futurerestore-Linux-x86_64-RELEASE.zip
         xz -dc futurerestore-*.xz | tar xfv -
-        rm -rf linux_fix.sh
     fi
     mv futurerestore "$dir"/
     rm -rf futurerestore-*
